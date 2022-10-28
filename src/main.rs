4bb9eacade88c3da80e2fc53942d73a19b5087bd
Donal Shortt <donalshortt@donals.eu>
2022-10-28 15:28:30 +0200

    latest

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ use std::fs::File;
 use std::io::{BufRead, Cursor};
 use std::io::prelude::*;
 
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 fn main() {
-    let mut savefile = File::open("../saves/mp_autosave.eu4")
+    let mut savefile = File::open("/home/donal/projects/mocd/saves/mp_autosave.eu4")
         .expect("file opened");
     let mut savefile_string = String::new();
     
-    savefile.read_to_string(&mut savefile_string)
-        .expect("savefile stringified");
+    savefile.read_to_string(&mut savefile_string).expect("savefile stringified");
+
+    println!("Hello World!");
 }
