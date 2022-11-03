745615766a11fdaf9475517cebe0c1a959d6eac3
Donal Shortt <donalshortt@donals.eu>
2022-11-03 20:37:02 +0100

    latest

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::fs::File;
-use std::io::{BufRead, Cursor};
 use std::io::prelude::*;
-
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
 
 fn main() {
     let mut savefile = File::open("/home/donal/projects/mocd/saves/mp_autosave.eu4")
         .expect("file opened");
-    let mut savefile_string = String::new();
-    
-    savefile.read_to_string(&mut savefile_string).expect("savefile stringified");
+
+    let mut buf = Vec::new();
+    savefile.read_to_end(&mut buf)
+        .expect("savefile vectored");
+
+    let savefile_contents = String::from_utf8_lossy(&buf);
+
+    let players_countries_index = savefile_contents.find("players_countries");
+
+
 
     println!("Hello World!");
 }
