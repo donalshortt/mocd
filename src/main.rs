e11374db7d93bcd527a1c0388bc0a0ff3185d2b9
donal <donalshortt@donals.eu>
2022-11-14 23:21:01 +0000

    latest

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
 use std::fs::File;
+use std::io::BufReader;
 use std::io::prelude::*;
 
 fn main() {
     let mut savefile = File::open("/home/donal/projects/mocd/saves/mp_autosave.eu4")
         .expect("file opened");
-
-    let mut buf = Vec::new();
-    savefile.read_to_end(&mut buf)
-        .expect("savefile vectored");
-
-    let savefile_contents = String::from_utf8_lossy(&buf);
-
-    let players_countries_index = savefile_contents.find("players_countries");
 
 
 
