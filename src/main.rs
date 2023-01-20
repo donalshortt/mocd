325690a86fb1fcc367e395dfbf835fdb7868c253
Donal Shortt <donalshortt@donals.eu>
2023-01-20 18:42:46 +0100

    polls for changes, but will break if the program exits

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,27 @@ use std::{thread, time};
 // every 20 seconds, check if the metadata last modified has changed
 // if yes, do the roar,
 // if no, wait another 20 seconds
+// take into account that this function might exited and restarted
 
-fn main() -> std::io::Result<()> {
+fn main() {
     env::set_var("RUST_BACKTRACE", "1");
     let mut game_data = mocb::Game::default();
     let filepath = "/home/donal/projects/moc/mocp/saves/mp_autosave.eu4";
-    let last_metadata;
+    let mut last_metadata: time::SystemTime = time::SystemTime::now();
 
     loop {
-        if (latest_metadata != last_metadata) {
-            let latest_metadata = fs::metadata(filepath)?;
+        let latest_metadata = fs::metadata(filepath).unwrap().modified().unwrap();
+        
+        if latest_metadata != last_metadata {
+            last_metadata = latest_metadata;
 
             parser::parse(filepath, &mut game_data);
             sender::send(&game_data);
+
+            println!("Parsed and sent!");
         } else {
-            thread::sleep(10000);
+            println!("Sleeping....");
+            thread::sleep(time::Duration::new(5, 0));
         }
     }
-
-    Ok(())
 }
